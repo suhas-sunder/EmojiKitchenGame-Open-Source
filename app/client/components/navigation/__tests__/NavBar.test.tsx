296903b40dd138298ebcc6c25cc7eda7d9823d54
Suhas Sunder <suhas@live.ca>
2024-11-14 17:33:51 -0500

    refactor: tests
    -Mock render and add appropriat text for each test case.

--- app/client/components/navigation/__tests__/NavBar.test.tsx
+++ app/client/components/navigation/__tests__/NavBar.test.tsx
@@ -1,64 +1,57 @@
-import '@testing-library/jest-dom';
-import { describe, it, expect } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import NavBar from  '../NavBar';
+import "@testing-library/jest-dom";
+import { describe, it, expect, beforeEach } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import NavBar from "../NavBar";
 
-describe('NavBar Component', () => {
-    it('render the NavBar Component', () => {
-        render((
-            <MemoryRouter>
-                <NavBar />
-            </MemoryRouter>
-        ));
+//Mocking the render and calling it before each test prevents code from being repeated. You can pass props to the component when needed.
+const MockNavBar = () => {
+  render(
+    <MemoryRouter>
+      <NavBar />
+    </MemoryRouter>
+  );
+};
 
-        expect(screen.getByTestId('burger-icons')).toBeInTheDocument;
-    });
+beforeEach(() => {
+  MockNavBar();
+});
 
-    it('checks upon initialization that the menu is hidden', () => {
-        render((<MemoryRouter>
-            <NavBar />
-        </MemoryRouter>));
+describe("NavBar Component", () => {
+  //Should render navbar (use getByRole and look for "navigation" role. Always try to use getByRole whenever possible.)
 
-        expect(document.body.classList.contains("overlfow-y-hidden")).toBe(false);
-    });
+  //Should render nav links
 
-    it('shows menu when burger-icons clicked', () => {
-        render((
-            <MemoryRouter>
-                <NavBar />
-            </MemoryRouter>
-        ));
+  //Should render logo
 
-        const BurgerIcon = screen.getByTestId('burger-icons');
-        fireEvent.click(BurgerIcon);
-        expect(document.body.classList.contains("overflow-y-hidden")).toBe(true);
-    });
+  //(when writing tests that start with "it", follow it with the word "should") If your using "test" instead of "it", then start with "if" or something along those lines.
 
-    it('hides the menu when a link is clicked', () => {
-        render((
-            <MemoryRouter>
-                <NavBar />
-            </MemoryRouter>
-        ));
+  it("should render burger icons", () => {
+    expect(screen.getByTestId("burger-icons")).toBeInTheDocument();
+  });
 
-        const link = screen.getByText(/Emoji Copy and Paste/i);
-        fireEvent.click(link);
-        expect(document.body.classList.contains("overflow-y-hidden")).toBe(false);
-    });
+  it("should check upon initialization that the menu is hidden", () => {
+    expect(document.body.classList.contains("overlfow-y-hidden")).toBe(false);
+  });
 
-    it('hides the menu when the screen is resized', () => {
-        render((
-            <MemoryRouter>
-                <NavBar />
-            </MemoryRouter>
-        ));
+  it("should show menu when burger-icons clicked", () => {
+    const BurgerIcon = screen.getByTestId("burger-icons");
+    fireEvent.click(BurgerIcon);
+    expect(document.body.classList.contains("overflow-y-hidden")).toBe(true);
+  });
 
-        const BurgerIcon = screen.getByTestId('burger-icons');
-        fireEvent.click(BurgerIcon);
-        expect(document.body.classList.contains("overflow-y-hidden")).toBe(true);
+  it("should hide the menu when a link is clicked", () => {
+    const link = screen.getByText(/Emoji Copy and Paste/i);
+    fireEvent.click(link);
+    expect(document.body.classList.contains("overflow-y-hidden")).toBe(false);
+  });
 
-        fireEvent(window, new Event("resize"));
-        expect(document.body.classList.contains("overflow-y-hidden")).toBe(false);
-    })
+  it("should hide the menu when the screen is resized", () => {
+    const BurgerIcon = screen.getByTestId("burger-icons");
+    fireEvent.click(BurgerIcon);
+    expect(document.body.classList.contains("overflow-y-hidden")).toBe(true);
+
+    fireEvent(window, new Event("resize"));
+    expect(document.body.classList.contains("overflow-y-hidden")).toBe(false);
+  });
 });
