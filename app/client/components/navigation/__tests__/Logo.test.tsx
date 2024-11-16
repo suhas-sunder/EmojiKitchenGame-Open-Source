2dab5113a8e0ed0008e17fc53ddd601d7f9c05ab
S0han <kane.pereira18@gmail.com>
2024-11-16 00:59:59 -0500

    setup: create and setup test file for the logo component

---
+++ app/client/components/navigation/__tests__/Logo.test.tsx
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Logo from "../Logo";
+
+const MockLogo = () => {
+    const setShowMobileMenu = vi.fn();
+    render(
+        <MemoryRouter>
+            <Logo setShowMobileMenu={setShowMobileMenu} />
+        </MemoryRouter>
+    );
+}
+
+beforeEach(() => {
+    MockLogo()
+});
