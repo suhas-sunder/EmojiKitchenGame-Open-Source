57258a30670ee14c1118c35157c1103425ce1be4
S0han <kane.pereira18@gmail.com>
2024-11-18 14:07:09 -0500

    setup: create mock for navigation hook

--- app/client/components/navigation/__tests__/Footer.test.tsx
+++ app/client/components/navigation/__tests__/Footer.test.tsx
@@ -1,8 +1,18 @@
 import "@testing-library/jest-dom";
 import { describe, it, expect, beforeEach, vi } from "vitest";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, useNavigate } from "react-router-dom";
 import Footer from "../Footer";
+
+vi.mock("react-router-dom", async () => {
+    const actualRouter = await vi.importActual('react-router-dom');
+    return {
+        ...actualRouter,
+        useNavigate: vi.fn(),
+    };
+});
+
+const mockNavigate = vi.fn();
 
 const MockFooter = () => {
    render(
