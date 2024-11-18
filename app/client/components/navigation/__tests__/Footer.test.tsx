b4e6d75f892094870fcabd240b7edfb5e7ea547b
S0han <kane.pereira18@gmail.com>
2024-11-18 12:36:57 -0500

    test: should render the link sitemap

--- app/client/components/navigation/__tests__/Footer.test.tsx
+++ app/client/components/navigation/__tests__/Footer.test.tsx
@@ -5,15 +5,26 @@ import { MemoryRouter } from "react-router-dom";
 import Footer from "../Footer";
 
 const MockFooter = () => {
-    <MemoryRouter>
-        <Footer /> 
-    </MemoryRouter>
+   render(
+        <MemoryRouter>
+            <Footer /> 
+        </MemoryRouter>
+    );
 };
 
 beforeEach(() => {
+    vi.clearAllMocks()
     MockFooter();
 });
 
-describe("Render render footer", () => {
-    it("", () => {});
+describe("Render footer", () => {
+    it("should render text EmojiKitchenGame", () => {
+        const FooterText = screen.getByText(/EmojiKitchenGame/i);
+        expect(FooterText).toBeInTheDocument();
+    });
+
+    it("should render the link Sitemap", () => {
+        const FooterLink = screen.getByRole("link", {name: /Sitemap/i});
+        expect(FooterLink).toBeInTheDocument();
+    });
 });
