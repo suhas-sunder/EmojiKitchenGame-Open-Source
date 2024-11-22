b7b71f79feb204b2999ebc4800ceb5ec281acc2d
S0han <kane.pereira18@gmail.com>
2024-11-22 01:11:34 -0500

    setup: creating the test file for SectionMenu component

--- app/client/components/navigation/__tests__/Footer.test.tsx
+++ app/client/components/navigation/__tests__/Footer.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { describe, it, expect, beforeEach, vi } from "vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import Footer from "../Footer";
 
