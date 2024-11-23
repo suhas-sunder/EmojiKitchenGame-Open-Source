e5b2e95e38c7279318c375c7dc9dc4f46824d2ff
S0han <kane.pereira18@gmail.com>
2024-11-23 02:06:18 -0500

    setup: render component mock with props

--- app/client/components/navigation/__tests__/SectionMenu.test.tsx
+++ app/client/components/navigation/__tests__/SectionMenu.test.tsx
@@ -3,3 +3,13 @@ import { describe, it, expect, beforeEach, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import SectionMenu from "../SectionMenu";
+
+const props = {test: "props to the troll"};
+
+const MockSectionMenu = () => {
+    render(
+        <MemoryRouter>
+            <SectionMenu object={props} />
+        </MemoryRouter>
+    );
+}
