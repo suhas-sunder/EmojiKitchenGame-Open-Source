edca42ac8d1d1fd8a41f2f00c7cc1604ce4b2af4
S0han <kane.pereira18@gmail.com>
2024-11-28 00:24:48 -0500

    setup: create test file and add import for Icon in utils

---
+++ app/client/components/utils/other/__tests__/Icon.test.tsx
@@ -0,0 +1,4 @@
+import "@testing-library/jest-dom";
+import { describe, it, expect, beforeEach, vi} from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Icon from "../Icon";
