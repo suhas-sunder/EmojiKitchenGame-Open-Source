636b41ccaba4438ffed5e1c911a7afa2f06c81f9
S0han <kane.pereira18@gmail.com>
2024-12-06 16:47:26 -0500

    cleanup: remove unecessary mockitems that arent used in test

--- app/client/components/utils/other/__tests__/Icon.test.tsx
+++ app/client/components/utils/other/__tests__/Icon.test.tsx
@@ -1,31 +1,12 @@
 import "@testing-library/jest-dom";
 import { describe, it, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Icon from "../Icon";
-
-const MockCopyIcon = () => (
-    <svg
-      focusable="false"
-      aria-hidden="true"
-      viewBox="0 0 24 24"
-      data-testid="ContentCopyRoundedIcon"
-      width="24"
-      height="24"
-    >
-      <path d="M15 20H5V7c0-.55-.45-1-1-1s-1 .45-1 1v13c0 1.1.9 2 2 2h10c.55 0 1-.45 1-1s-.45-1-1-1m5-4V4c0-1.1-.9-2-2-2H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2m-2 0H9V4h9z"></path>
-    </svg>
-);
-
-const MockIconComponents = {
-    copy: MockCopyIcon,
-}
-
-type MockIconName = keyof typeof MockIconComponents
+import Icon, { IconName } from "../Icon";
 
 const props = {
     title: "Dummy Title",
     customStyle: "super-sonic-style",
-    icon: "copy" as MockIconName
+    icon: "copy" as IconName
 }
 
 const MockIcon = () => {
