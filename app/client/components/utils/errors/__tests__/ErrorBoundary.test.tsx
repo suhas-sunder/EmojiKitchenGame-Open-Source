db287c55555b7904cc3a00a7c2e06c1a5b3b6976
S0han <kane.pereira18@gmail.com>
2024-12-09 00:23:57 -0500

    setup: import test packages for ErrorBoundary

---
+++ app/client/components/utils/errors/__tests__/ErrorBoundary.test.tsx
@@ -0,0 +1,4 @@
+import "@testing-library/jest-dom";
+import { describe, it, expect, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import ErrorBoundary from "../ErrorBoundary";
